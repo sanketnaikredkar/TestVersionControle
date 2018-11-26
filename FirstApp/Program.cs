9f2c7c83e401c7ca329b6244353163bc6838db70
Sanket <44740557+sanketnaikredkar@users.noreply.github.com>
2018-11-26 14:40:45 +0530

    Update Program.cs

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,7 +7,7 @@ namespace FirstApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Branch 1"); //edited : GitHub 3
+            Console.WriteLine("Branch 1"); //edited : GitHub 4
         }
     }
 }
